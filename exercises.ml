1c5eb818224bfa7ad89f134d4cba1b1655d00b47
Eric Lee <ericlovesmath@gmail.com>
2023-08-08 14:55:37 -0700

    Problems 14-17 of 99 OCaml Problems

--- exercises.ml
+++ exercises.ml
@@ -248,3 +248,113 @@ module Problem13 = struct
     assert (fails (fun () -> drop [ "a"; "b" ] 0));
     assert (fails (fun () -> drop [ "a"; "b" ] (-1)))
 end
+
+module Problem14 = struct
+  (** Split a list into two parts; the length of the first part is given. If the
+      length of the first part is longer than the entire list, then the first
+      part is the list and the second part is empty. *)
+  let split lst len =
+    let rec split' lst acc len =
+      match (len, lst) with
+      | 0, _ | _, [] -> (List.rev acc, lst)
+      | len, e :: rem -> split' rem (e :: acc) (len - 1)
+    in
+    if len < 0
+    then failwith "Problem14.split received negative len"
+    else split' lst [] len
+
+  let () =
+    assert (
+      split [ "a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j" ] 3
+      = ([ "a"; "b"; "c" ], [ "d"; "e"; "f"; "g"; "h"; "i"; "j" ]));
+    assert (split [ "a"; "b"; "c"; "d" ] 5 = ([ "a"; "b"; "c"; "d" ], []));
+    assert (split [ "a"; "b"; "c" ] 0 = ([], [ "a"; "b"; "c" ]));
+    assert (split [] 0 = ([], []));
+    assert (split [] 10 = ([], []));
+    assert (fails (fun () -> split [ "a"; "b" ] (-1)))
+end
+
+module Problem15 = struct
+  (** Extract a Slice From a List, inclusive. Fails if not in bounds*)
+  let slice lst left right =
+    let rec drop lst len =
+      match (lst, len) with
+      | _, 0 -> lst
+      | [], _ -> failwith "Problem15.slice: Left bound longer than list"
+      | _ :: rem, _ -> drop rem (len - 1)
+    in
+    let keep lst len =
+      let rec keep' lst acc len =
+        match (lst, len) with
+        | _, 0 -> List.rev acc
+        | [], _ -> failwith "Problem15.slice: Right bound longer than list"
+        | e :: rem, _ -> keep' rem (e :: acc) (len - 1)
+      in
+      keep' lst [] len
+    in
+    match (left, right) with
+    | l, _ when l < 0 -> failwith "Problem15.slice: Negative left bound"
+    | l, r when l > r -> failwith "Problem15.slice: Left bound > Right bound"
+    | l, r -> keep (drop lst left) (right - left + 1)
+
+  let () =
+    assert (
+      slice [ "a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j" ] 2 6
+      = [ "c"; "d"; "e"; "f"; "g" ]);
+    assert (slice [ "a"; "b"; "c"; "d" ] 0 3 = [ "a"; "b"; "c"; "d" ]);
+    assert (slice [ "a"; "b"; "c"; "d" ] 1 1 = [ "b" ]);
+    assert (fails (fun () -> slice [] 0 0 = []));
+    assert (fails (fun () -> slice [ "a"; "b"; "c"; "d" ] 1 6));
+    assert (fails (fun () -> slice [ "a"; "b"; "c"; "d" ] (-1) 3));
+    assert (fails (fun () -> slice [ "a"; "b"; "c"; "d" ] 2 1));
+    assert (fails (fun () -> slice [] (-1) 0));
+    assert (fails (fun () -> slice [] 0 1))
+end
+
+module Problem16 = struct
+  (** Rotate a list N places to the left. *)
+
+  let rotate lst n =
+    match lst with
+    | [] -> []
+    | lst ->
+      let left, right = Problem14.split lst (n mod List.length lst) in
+      right @ left
+
+  let () =
+    assert (
+      rotate [ "a"; "b"; "c"; "d"; "e"; "f"; "g"; "h" ] 3
+      = [ "d"; "e"; "f"; "g"; "h"; "a"; "b"; "c" ]);
+    assert (
+      rotate [ "a"; "b"; "c"; "d"; "e"; "f"; "g"; "h" ] 8
+      = [ "a"; "b"; "c"; "d"; "e"; "f"; "g"; "h" ]);
+    assert (rotate [ "a"; "b"; "c" ] 0 = [ "a"; "b"; "c" ]);
+    assert (rotate [ "a"; "b"; "c" ] 5 = [ "c"; "a"; "b" ]);
+    assert (rotate [ "a" ] 5 = [ "a" ]);
+    assert (rotate [] 5 = [])
+end
+
+module Problem17 = struct
+  (** Remove the K'th element from a list, error if out of bounds *)
+  let remove_at k lst =
+    let fail_index_oob () =
+      failwith "Problem17.remove_at: Index out of bounds"
+    in
+    let rec remove_at' acc k lst =
+      match (k, lst) with
+      | 0, _ :: rem -> List.rev acc @ rem
+      | _, [] -> fail_index_oob ()
+      | k, e :: rem -> remove_at' (e :: acc) (k - 1) rem
+    in
+    if k < 0 then fail_index_oob () else remove_at' [] k lst
+
+  let () =
+    assert (remove_at 0 [ "a"; "b"; "c"; "d" ] = [ "b"; "c"; "d" ]);
+    assert (remove_at 1 [ "a"; "b"; "c"; "d" ] = [ "a"; "c"; "d" ]);
+    assert (remove_at 2 [ "a"; "b"; "c"; "d" ] = [ "a"; "b"; "d" ]);
+    assert (remove_at 3 [ "a"; "b"; "c"; "d" ] = [ "a"; "b"; "c" ]);
+    assert (fails (fun () -> remove_at 4 [ "a"; "b"; "c"; "d" ]));
+    assert (fails (fun () -> remove_at (-1) [ "a"; "b"; "c"; "d" ]));
+    assert (remove_at 0 [ "a" ] = []);
+    assert (fails (fun () -> remove_at 0 []))
+end
